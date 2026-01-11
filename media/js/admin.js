2f09df08339d9d9ed978a448f0e8d99e53a2908e
Sergey Tolkachyov <musstudent@mail.ru>
2026-01-11 14:55:11 +0400

    v.2.3.0

--- media/js/admin.js
+++ media/js/admin.js
@@ -1,8 +1,8 @@
 /**
  * @package       View logs
- * @version       2.2.0.1
+ * @version       2.3.0
  * @Author        Sergey Tolkachyov, https://web-tolk.ru
- * @copyright     Copyright (c) 2019 - 2025 Sergey Tolkachyov. All rights reserved.
+ * @copyright     Copyright (c) 2019 - 2026 Sergey Tolkachyov. All rights reserved.
  * @license       GNU/GPL3 http://www.gnu.org/licenses/gpl-3.0.html
  * @since         1.0.0
  */
